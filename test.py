32f1792f43ef581bae00d007fc1c4f8c6f5bf91a
mcpar-land <john@mcpar.land>
2024-02-12 13:42:56 -0500

    minor version incr

--- test.py
+++ test.py
@@ -3,7 +3,11 @@ import polars as pl
 import time
 
 start = time.time()
-df = pl.scan_parquet(sys.argv[1], low_memory=True)
+df = pl.scan_parquet(sys.argv[1], low_memory=True).filter(
+    pl.col("name").str.ends_with(".xml")
+).with_columns(
+    body_str=pl.col("body").cast(pl.Utf8)
+)
 
 print(df.collect())
 end = time.time()
